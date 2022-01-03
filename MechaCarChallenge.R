b568e0afa0f2d55177a2f49d0bab7243d7972794
Megan Riley <90050622+MeganKRiley@users.noreply.github.com>
2022-01-02 18:40:43 -0600

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,4 +5,7 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) #D2 step 2
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #D2 step 3
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #D2 step 4                                   
-                                               
+t.test(suspension_coil$PSI,mu=1500) #D3 step 1                                   
+t.test(subset(suspension_coil$PSI, suspension_coil$Manufacturing_Lot=="Lot1"),mu=1500) #D3 step 2
+t.test(subset(suspension_coil$PSI, suspension_coil$Manufacturing_Lot=="Lot2"),mu=1500) #D3 step 2
+t.test(subset(suspension_coil$PSI, suspension_coil$Manufacturing_Lot=="Lot3"),mu=1500) #D3 step 2
